3ffcfcb5d32544a1f947f773c58aabe48e84b225
Vasilije Pantic <vpantic10@gmail.com>
2021-03-05 20:16:56 +0100

    implementing the transformer model

---
+++ training/transformer_model.py
@@ -0,0 +1,246 @@
+from transformer_util import *
+import tensorflow as tf
+
+
+class EncoderLayer(tf.keras.layers.Layer):
+    """
+    Encoder layer consists of multi-head attention followed by normalization layer and point wised feed forward network
+    followed with normalization layer
+    """
+
+    def __init__(self, d_model, number_of_heads, dff, rate=0.1):
+        """
+        Constructor for encoder layer
+
+        :param d_model: dimension of the word embedding vector
+        :param number_of_heads: number of heads to work in parallel
+        :param dff: inner-layer dimensionality
+        :param rate: dropout rate
+        """
+
+        super(EncoderLayer, self).__init__()
+        self.multi_head_attention = MultiHeadAttention(d_model, number_of_heads)
+        self.feed_forward_network = point_wise_feed_forward_network(d_model, dff)
+        self.normalization_layer1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.normalization_layer2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, mask):
+        """
+        Call function for the encoder layer
+
+        :param x: input vector
+        :param training: boolean values to do a dropout only when is it a training process
+        :param mask: mask for multi-head attention
+        :return: output of the encoder layer
+        """
+
+        attention_output, _ = self.multi_head_attention(x, x, x, mask)
+        attention_output = self.dropout1(attention_output, training=training)
+        out1 = self.normalization_layer1(x + attention_output)
+        feed_forward_network_output = self.feed_forward_network(out1)
+        feed_forward_network_output = self.dropout2(feed_forward_network_output, training=training)
+        out2 = self.normalization_layer2(out1 + feed_forward_network_output)
+        return out2
+
+
+class DecoderLayer(tf.keras.layers.Layer):
+    """
+    Decoder layer consists of masked multi-head attention followed by normalization layer, multi-head attention followed
+    by normalization layer and point wise feed forward network followed by normalization layer
+    """
+
+    def __init__(self, d_model, number_of_heads, dff, rate=0.1):
+        """
+        Constructor for decoder layer
+
+        :param d_model: dimension of the word embedding vector
+        :param number_of_heads: number of heads to work in parallel
+        :param dff: inner-layer dimensionality
+        :param rate: dropout rate
+        """
+
+        super(DecoderLayer, self).__init__()
+        self.masked_multi_head_attention = MultiHeadAttention(d_model, number_of_heads)
+        self.multi_head_attention = MultiHeadAttention(d_model, number_of_heads)
+        self.feed_forward_network = point_wise_feed_forward_network(d_model, dff)
+        self.normalization_layer1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.normalization_layer2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.normalization_layer3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+        self.dropout3 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
+        """
+        Call function for the decoder layer
+
+        :param x: input vector
+        :param encoder_output: output of the encoder
+        :param training: boolean values to do a dropout only when is it a training process
+        :param look_ahead_mask: look ahead mask for masked multi-head attention
+        :param padding_mask: padding mask for multi-head attention
+        :return: output of the decoder layer
+        """
+
+        attention1, attention_weights_block1 = self.masked_multi_head_attention(x, x, x, look_ahead_mask)
+        attention1 = self.dropout1(attention1, training=training)
+        out1 = self.normalization_layer1(attention1 + x)
+        attention2, attention_weights_block2 = self.multi_head_attention(encoder_output, encoder_output, out1,
+                                                                         padding_mask)
+        attention2 = self.dropout2(attention2, training=training)
+        out2 = self.normalization_layer2(attention2 + out1)
+        feed_forward_network_output = self.feed_forward_network(out2)
+        feed_forward_network_output = self.dropout3(feed_forward_network_output, training=training)
+        out3 = self.normalization_layer3(feed_forward_network_output + out2)
+        return out3, attention_weights_block1, attention_weights_block2
+
+
+class Encoder(tf.keras.layers.Layer):
+    """
+    Encoder class contains of word embedding, positional encoding and several numbers of encoder layers to represent an
+    encoder of transformer model
+    """
+
+    def __init__(self, number_of_layers, d_model, number_of_heads, dff, input_vocabulary_size,
+                 maximum_position_encoding, rate=0.1):
+        """
+        Constructor for the Encoder
+
+        :param number_of_layers: number of encoder layers
+        :param d_model: dimension of the word embedding vector
+        :param number_of_heads: number of heads to work in parallel
+        :param dff: inner-layer dimensionality
+        :param input_vocabulary_size: size of the input vocabulary
+        :param maximum_position_encoding: maximum for positional encoding
+        :param rate: dropout rate
+        """
+
+        super(Encoder, self).__init__()
+        self.d_model = d_model
+        self.number_of_layers = number_of_layers
+        self.embedding = tf.keras.layers.Embedding(input_vocabulary_size, d_model)
+        self.positional_encoding = positional_encoding(maximum_position_encoding, d_model)
+        self.encoder_layers = [EncoderLayer(d_model, number_of_heads, dff, rate) for _ in range(number_of_layers)]
+        self.dropout = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, mask):
+        """
+        Call function of the Encoder
+
+        :param x: input vector
+        :param training: boolean values to do a dropout only when is it a training process
+        :param mask: mask for multi-head attention for encoder layers
+        :return: output vector
+        """
+
+        sequence_length = tf.shape(x)[1]
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x += self.positional_encoding[:, :sequence_length, :]
+        x = self.dropout(x, training=training)
+        for i in range(self.num_layers):
+            x = self.encoder_layers[i](x, training, mask)
+        return x
+
+
+class Decoder(tf.keras.layers.Layer):
+    """
+    Decoder class contains of word embedding, positional encoding and several numbers of decoder layers to represent an
+    decoder of transformer model
+    """
+
+    def __init__(self, number_of_layers, d_model, number_of_heads, dff, target_vocabulary_size,
+                 maximum_position_encoding, rate=0.1):
+        """
+        Constructor for the Decoder
+
+        :param number_of_layers: number of decoder layers
+        :param d_model: dimension of the word embedding vector
+        :param number_of_heads: number of heads to work in parallel
+        :param dff: inner-layer dimensionality
+        :param target_vocabulary_size: size of the target vocabulary
+        :param maximum_position_encoding: maximum for positional encoding
+        :param rate: dropout rate
+        """
+
+        super(Decoder, self).__init__()
+        self.d_model = d_model
+        self.number_of_layers = number_of_layers
+        self.embedding = tf.keras.layers.Embedding(target_vocabulary_size, d_model)
+        self.positional_encoding = positional_encoding(maximum_position_encoding, d_model)
+        self.decoder_layers = [DecoderLayer(d_model, number_of_heads, dff, rate) for _ in range(number_of_layers)]
+        self.dropout = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
+        """
+        Call function of the Decoder
+
+        :param x: input vector
+        :param encoder_output: output of the encoder
+        :param training: boolean values to do a dropout only when is it a training process
+        :param look_ahead_mask: look ahead mask for masked multi-head attention
+        :param padding_mask: padding mask for multi-head attention
+        :return: output of the decoder
+        """
+
+        sequence_length = tf.shape(x)[1]
+        attention_weights = {}
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x += self.positional_encoding[:, :sequence_length, :]
+        x = self.dropout(x, training=training)
+        for i in range(self.num_layers):
+            x, block1, block2 = self.decoder_layers[i](x, encoder_output, training, look_ahead_mask, padding_mask)
+            attention_weights['decoder_layer{}_block1'.format(i + 1)] = block1
+            attention_weights['decoder_layer{}_block2'.format(i + 1)] = block2
+        return x, attention_weights
+
+
+class Transformer(tf.keras.Model):
+    """
+    Transformer class that consist of Encoder class, Decoder class and one final layer
+    """
+
+    def __init__(self, number_of_layers, d_model, number_of_heads, dff, input_vocabulary_size, target_vocabulary_size,
+                 positional_encoding_input, positional_encoding_target, rate=0.1):
+        """
+        Constructor of the Transformer
+
+        :param number_of_layers: number of Encoder/Decoder layers
+        :param d_model: dimension of the word embedding vector
+        :param number_of_heads: number of heads to work in parallel
+        :param dff: inner-layer dimensionality
+        :param input_vocabulary_size: size of the input vocabulary
+        :param target_vocabulary_size: size of the target vocabulary
+        :param positional_encoding_input: maximum for input positional encoding
+        :param positional_encoding_target: maximum for target positional encoding
+        :param rate: dropout rate
+        """
+
+        super(Transformer, self).__init__()
+        self.encoder = Encoder(number_of_layers, d_model, number_of_heads, dff, input_vocabulary_size,
+                               positional_encoding_input, rate)
+        self.decoder = Decoder(number_of_layers, d_model, number_of_heads, dff, target_vocabulary_size,
+                               positional_encoding_target, rate)
+        self.final_layer = tf.keras.layers.Dense(target_vocabulary_size)
+
+    def call(self, input, target, training, encoder_padding_mask, look_ahead_mask, decoder_padding_mask):
+        """
+        Call function for the Transformer
+
+        :param input: input sequence
+        :param target: output sequence
+        :param training: boolean values to do a dropout only when is it a training process
+        :param encoder_padding_mask: padding mask for encoder
+        :param look_ahead_mask: look ahead mask for decoder
+        :param decoder_padding_mask: padding mask for decoder
+        :return: output of the transformer
+        """
+
+        encoder_output = self.encoder(input, training, encoder_padding_mask)
+        decoder_output, attention_weights = self.decoder(target, encoder_output, training, look_ahead_mask,
+                                                         decoder_padding_mask)
+        final_output = self.final_layer(decoder_output)
+        return final_output, attention_weights
---
+++ training/transformer_util.py
@@ -0,0 +1,155 @@
+import numpy as np
+import tensorflow as tf
+
+
+def get_angles(position, i, d_model):
+    """
+    Creates an angle that will be used for calculating the positional encoding
+
+    :param position: word position in the text
+    :param i: position in the word embedding vector
+    :param d_model: dimension of the word embedding vector
+    :return: angle for the positional encoding
+    """
+
+    return position * (1.0 / np.power(10000, (2 * (i // 2)) / np.float32(d_model)))
+
+
+def positional_encoding(position, d_model):
+    """
+    Creating a positional encoding that will be added to the word embedding vector in order to keep the information
+    about the position of the word in the text
+
+    :param position: word position in the text
+    :param d_model: dimension of the word embedding vector
+    :return: positional encoding
+    """
+
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis], np.arange(d_model)[np.newaxis, :], d_model)
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    pos_encoding = angle_rads[np.newaxis, ...]
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+def create_padding_mask(seq):
+    """
+    Padding mask for masking "pad" sequences
+
+    :param seq: input sequence
+    :return: masked sequence
+    """
+
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    return seq[:, tf.newaxis, tf.newaxis, :]
+
+
+def create_lookahead_mask(size):
+    """
+    Lookahead mask for masking future words from contributing in prediction of current words in self attention
+
+    :param size: size of the requested mask
+    :return: mask
+    """
+
+    return 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+
+
+def scaled_dot_product_attention(query, key, value, mask):
+    """
+    Scaled dot product attention is defined as softmax(QK.transpose/sqrt(dk))*V
+
+    :param query: query vector  which is activated and trained when a word vector xn seeks all of the key-value pairs of the
+           other word vectors, including itself in self-attention
+    :param key: key vector which will be trained to provide an attention value
+    :param value: value vector which will be trained to provide another attention value
+    :param mask: mask to add to scaled Q and K product
+    :return: scaled dot product attention and attention weights
+    """
+
+    matmul_qk = tf.matmul(query, key, transpose_b=True)
+    dk = tf.cast(tf.shape(key)[-1], tf.float32)
+    scaled_qk_product = matmul_qk / tf.math.sqrt(dk)
+    if mask is not None:
+        scaled_qk_product += (mask * -1e9)
+    attention_weights = tf.nn.softmax(scaled_qk_product, axis=-1)
+    output = tf.matmul(attention_weights, value)
+    return output, attention_weights
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    """
+    Multi-head attention allows the model to jointly attend to information from different representation subspaces at
+    different positions. With a single attention head, averaging inhibits this
+    """
+
+    def __init__(self, d_model, number_of_heads):
+        """
+        Constructor for multi-head attention
+
+        :param d_model: dimension of the word embedding vector
+        :param number_of_heads: number of heads to work in parallel
+        """
+
+        super(MultiHeadAttention, self).__init__()
+        self.number_of_heads = number_of_heads
+        self.d_model = d_model
+        self.depth = d_model // self.num_heads
+        self.weights_query = tf.keras.layers.Dense(d_model)
+        self.weights_key = tf.keras.layers.Dense(d_model)
+        self.weights_value = tf.keras.layers.Dense(d_model)
+        self.dense = tf.keras.layers.Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        """
+        Splitting input into heads that will work in parallel
+
+        :param x: word embeddings
+        :param batch_size: size of one batch for one head
+        :return: reshaped input suitable for heads working in parallel
+        """
+
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, value, key, query, mask):
+        """
+        Call function for the multi-head attention
+
+        :param query: query vector  which is activated and trained when a word vector xn seeks all of the key-value pairs of the
+               other word vectors, including itself in self-attention
+        :param key: key vector which will be trained to provide an attention value
+        :param value: value vector which will be trained to provide another attention value
+        :param mask: mask to add to scaled Q and K product
+        :return: output of the multi-head attention and attention weights
+        """
+
+        batch_size = tf.shape(query)[0]
+        query = self.weights_query(query)
+        key = self.weights_key(key)
+        value = self.weights_value(value)
+        # splitting heads for query, key and value
+        query = self.split_heads(query, batch_size)
+        key = self.split_heads(key, batch_size)
+        value = self.split_heads(value, batch_size)
+        scaled_attention, attention_weights = scaled_dot_product_attention(query, key, value, mask)
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.d_model))
+        output = self.dense(concat_attention)
+        return output, attention_weights
+
+
+def point_wise_feed_forward_network(d_model, dff):
+    """
+    Classic fully connected feed-forward network which consists of two linear transformations with a ReLU activation in
+    between
+
+    :param d_model: dimension of the word embedding vector
+    :param dff: inner-layer dimensionality
+    :return: output of the point wise feed forward network
+    """
+
+    return tf.keras.Sequential([
+        tf.keras.layers.Dense(dff, activation='relu'),
+        tf.keras.layers.Dense(d_model)
+    ])
